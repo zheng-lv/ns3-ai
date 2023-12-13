29780163dc6f0d3fe860765160fa0b8d31aea762
zheng-lv <82482684+zheng-lv@users.noreply.github.com>
2023-12-13 16:47:59 +0800

    Update rl_tcp_py.cc

--- examples/rl-tcp/use-msg/rl_tcp_py.cc
+++ examples/rl-tcp/use-msg/rl_tcp_py.cc
@@ -15,6 +15,10 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Author:  Muyuan Shen <muyuan_shen@hust.edu.cn>
+ 这个 Python 模块ns3ai_rltcp_msg_py将 ns3::TcpRlEnv 和 ns3::TcpRlAct 结构体以及 Ns3AiMsgInterfaceImpl 类进行了封装，以便在 Python 中进行操作。
+它定义了一个 PyEnvStruct 类来表示 ns3::TcpRlEnv 结构体，并提供了属性访问器（def_readwrite）来读写结构体的成员变量。类似地，它定义了一个 PyActStruct 类来表示 ns3::TcpRlAct 结构体。
+此外，它定义了一个 Ns3AiMsgInterfaceImpl 类，该类是 ns3::Ns3AiMsgInterfaceImpl<ns3::TcpRlEnv, ns3::TcpRlAct> 类的 Python 包装。这个类提供了方法来在 Python 中处理与 TcpRlEnv 和 TcpRlAct 相关的消息。
+这个模块的目的是为了在 Python 中与 ns3 的 TcpRlEnv 和 TcpRlAct 结构体进行交互，以便进行人工智能（AI）相关的操作。
  */
 
 #include "tcp-rl-env.h"
