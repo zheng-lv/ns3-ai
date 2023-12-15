8777357193184bc5fc86f5254fefffbffd735223
zheng-lv <82482684+zheng-lv@users.noreply.github.com>
2023-12-15 20:20:19 +0800

    Update ns3-ai-gym-env.h

--- model/gym-interface/cpp/ns3-ai-gym-env.h
+++ model/gym-interface/cpp/ns3-ai-gym-env.h
@@ -32,74 +32,74 @@ class OpenGymDataContainer;
 class OpenGymInterface;
 
 /**
- * \brief Base class to create Gymnasium-compatible environments in C++ side.
+ * \brief Base class to create Gymnasium-compatible environments in C++ side.定义了一个名为 OpenGymEnv 的类，作为创建在C++中与Gymnasium兼容的环境的基类。
  */
 class OpenGymEnv : public Object
 {
   public:
-    OpenGymEnv();
-    ~OpenGymEnv() override;
+    OpenGymEnv();//构造函数 OpenGymEnv() 用于初始化对象的状态。
+    ~OpenGymEnv() override;//用于清理对象的资源。
 
-    static TypeId GetTypeId();
+    // static TypeId GetTypeId();/GetTypeId 是用于获取类型标识的静态成员函数，用于识别类的类型。
 
     /**
-     * Get action space (Box, Dict, ...) from simulation
+     * Get action space (Box, Dict, ...) from simulation 获取动作空间，例如 Box、Dict 等，从仿真中获取。
      */
     virtual Ptr<OpenGymSpace> GetActionSpace() = 0;
 
     /**
-     * Get observation space (Box, Dict, ...) from simulation
+     * Get observation space (Box, Dict, ...) from simulation 获取观察空间，例如 Box、Dict 等，从仿真中获取。
      */
     virtual Ptr<OpenGymSpace> GetObservationSpace() = 0;
 
     /**
      * Get whether game is over (simulation is finished).
-     * Normally, this should always return false.
+     * Normally, this should always return false. 取游戏是否结束（仿真是否完成）的标志。通常应始终返回false。
      */
     virtual bool GetGameOver() = 0;
 
     /**
-     * Get observation (stored in container)
+     * Get observation (stored in container) 获取存储在容器中的观察数据。
      */
     virtual Ptr<OpenGymDataContainer> GetObservation() = 0;
 
     /**
-     * Get reward
+     * Get reward 获取奖励。
      */
     virtual float GetReward() = 0;
 
     /**
-     * Get extra information
+     * Get extra information 获取额外信息。
      */
     virtual std::string GetExtraInfo() = 0;
 
     /**
-     * Execute actions. E.g., modify the contention window in TCP.
+     * Execute actions. E.g., modify the contention window in TCP. 执行动作，例如修改TCP中的拥塞窗口。
      */
     virtual bool ExecuteActions(Ptr<OpenGymDataContainer> action) = 0;
 
     /**
      * Sets the lower level gym interface (shared memory)
-     * associated to the environment
+     * associated to the environment 设置与环境关联的低级Gym接口（共享内存）
      */
     void SetOpenGymInterface(Ptr<OpenGymInterface> openGymInterface);
 
     /**
-     * Notify Python side about the states, and execute the actions
+     * Notify Python side about the states, and execute the actions  通知Python端关于状态，并执行动作。这是一个重要的函数，用于将环境的状态和动作信息发送到Python端。。
      */
     void Notify();
 
     /**
-     * Notify Python side that the simulation has ended.
+     * Notify Python side that the simulation has ended. 通知Python端仿真已经结束。
      */
     void NotifySimulationEnd();
 
   protected:
-    // Inherited
+    // Inherited 这两个函数用于初始化和处理资源。它们是从基类继承的虚拟函数。
     void DoInitialize() override;
     void DoDispose() override;
 
-    Ptr<OpenGymInterface> m_openGymInterface;
+    Ptr<OpenGymInterface> m_openGymInterface; m_openGymInterface 是指向 OpenGymInterface 对象的智能指针，用于与Python端通信。
 
   private:
 };
