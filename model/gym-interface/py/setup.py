b933285ba1b2b0de6f7eead2d7dd7be71e1ade64
zheng-lv <82482684+zheng-lv@users.noreply.github.com>
2023-12-17 21:36:23 +0800

    Update setup.py

--- model/gym-interface/py/setup.py
+++ model/gym-interface/py/setup.py
@@ -12,10 +12,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
+#这个脚本的目的是告诉 pip 安装此包所需的信息，以及指定依赖项。当用户运行 pip install . 或 pip install ns3ai_gym_env 时，将会根据这个脚本配置进行安装。
 # Author: Muyuan Shen <muyuan_shen@hust.edu.cn>
 
-
+#导入 setuptools 模块，用于构建和分发 Python 包。
 from setuptools import setup
 
 setup(
